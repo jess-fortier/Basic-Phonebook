5f2b59226a90b9b0a1b9a02d8c951795bdedebc6
Jess Fortier <tjfortier@gmail.com>
2015-01-13 14:08:35 -0800

    Corrected ref, removed superfluous attributes
    Deleted ng-required="true" attributes from form fields and corrected the
    reference to the angular local storage tool.

--- js/main.js
+++ js/main.js
@@ -2,8 +2,14 @@ var app = angular.module('phonebookApp', []);
 
 app.controller('mainController', function($scope) {
 	$scope.submitForm = function(isValid) {
+		alert('submitForm function has been called...');
 		if (isValid) {
 			alert('our form is amazing');
 		}
 	};
 });
+
+function saveEntry(name, phone){
+	localStorageService.clearAll();
+	localStorageService.set(name, phone);
+}
