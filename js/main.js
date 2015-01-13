759675119d8a4b68ae44d2fc825e30a28c100b50
Jess Fortier <tjfortier@gmail.com>
2015-01-13 12:49:21 -0800

    Form validation
    Added attribute level form validation via angular and dynamic error
    formatting.
    Changed to local instance/reference of angular.js.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 var app = angular.module('phonebookApp', []);
+
+app.controller('mainController', function($scope) {
+	$scope.submitForm = function(isValid) {
+		if (isValid) {
+			alert('our form is amazing');
+		}
+	};
+});
