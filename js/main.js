eebfa23589a47af74ac12c48a1ecf9c84b7bf849
Jess Fortier <tjfortier@gmail.com>
2015-01-13 21:17:41 -0800

    Init fields to localStor contents, add comments

--- js/main.js
+++ js/main.js
@@ -6,6 +6,11 @@ var app = angular
 	.controller('mainController', 
 		function($scope, localStorageService)
 		{
+			//Initialize fields to stored values, if any
+			$scope.entryName = localStorageService.get("name");
+			$scope.phoneNumber = localStorageService.get("phone");
+
+			//Commit the current field contents to localStorage
 			$scope.addEntry = function(isValid, name, phone)
 			{
 				if (isValid && localStorageService.isSupported)
@@ -14,14 +19,19 @@ var app = angular
 					localStorageService.set("phone", phone);
 				}
 			};
+			//Wipe name and phone entries for this app from localStorage
 			$scope.deleteEntry = function() {
 				localStorageService.remove("name");
 				localStorageService.remove("phone");
 			};
+
+			//Confirm whether entries for name or phone already exist in localStorage
 			$scope.hasEntry = function() {
 				if(localStorageService.get("name") == null && localStorageService.get("phone") == null)
 					return false;
 				return true;
 			};
+
+
 		}
 	);
